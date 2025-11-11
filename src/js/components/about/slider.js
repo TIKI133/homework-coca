bf9327c7f71ca11fe089a52c004591f97f4748be
TIKI <dima.tiki2000@mail.ru>
2025-11-11 13:12:59 +0300

    creared media phone

--- src/js/components/about/slider.js
+++ src/js/components/about/slider.js
@@ -1,15 +1,19 @@
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 
 export const useHeroSlider = () => {
   new Swiper('.hero__slider', {
     slidesPerView: 2.1,
     loop: true,
-    centeredSlides: true,
     breakpoints: {
+      321: {
+        slidesPerView: 1,
+      },
+      577: {
+        slidesPerView: 2,
+      },
       993: {
-        centeredSlides: false,
+        slidesPerView: 2.5,
       },
     },
   });
@@ -17,9 +21,16 @@ export const useHeroSlider = () => {
 
 export const useTeamSlider = () => {
   new Swiper('.team__slider', {
-    modules: [Navigation],
     slidesPerView: 3,
     spaceBetween: 22,
     loop: true,
+    breakpoints: {
+      321: {
+        slidesPerView: 1,
+      },
+      381: {
+        slidesPerView: 2,
+      },
+    },
   });
 };
